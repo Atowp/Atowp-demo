8dfd31eb7957dde5980ab94659b3e2afa19b46e6
Atowp <1461881720@qq.com>
2021-12-29 16:04:25 +0800

    start

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-
-module.exports = {
-    entry: './src/main.js',//入口
-    output: {//出口
-        filename: 'todos.bundle.js',
-        path: path.resolve(__dirname,'dist')
-    }
-}
